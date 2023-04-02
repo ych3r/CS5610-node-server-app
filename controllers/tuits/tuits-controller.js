2d129a8ee7eb4a0f4085dff3343989a055ad7ff5
ych3r <liuyc26@gmail.com>
2023-04-01 23:52:23 -0400

    add dislikes

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -6,6 +6,7 @@ const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = new Date().getTime() + "";
     newTuit.likes = 0;
+    newTuit.dislikes = 0;
     newTuit.liked = false;
     tuits.push(newTuit);
     res.json(newTuit);
