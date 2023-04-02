5f3070b8185d29a512d6bf62523512ff1892aa93
ych3r <liuyc26@gmail.com>
2023-04-01 22:42:35 -0400

    part 6

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import tuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 tuitsController(app);
 HelloController(app);
--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -5,7 +5,7 @@ export default [
     username: "SpaceX",
     handle: "@spacex",
     time: "2h",
-    image: "spacex.png",
+    image: "spacex.jpeg",
     title: "SpaceX's Mission",
     tuit: "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
     liked: true,
